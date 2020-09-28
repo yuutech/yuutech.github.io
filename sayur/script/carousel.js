c6ee13ebd6060a78397633b9fd5ee8592a29f258
yuutech <yusuf.supriyanto.07@gmail.com>
2020-09-29 02:41:06 +0700

    first commit

---
+++ sayur/script/carousel.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 100,
+  nav: false,
+  dots: false,
+  responsive: {
+    0: {
+      items: 4,
+    },
+    600: {
+      items: 5,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
